4b2c23acb276746611ed1973909d2b08364fd71b
oykusonmez <osonmez@purdue.edu>
2018-09-24 16:44:33 -0400

    StringUtils

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -3,10 +3,10 @@ public class BuggyUtilClient {
     public static void main(String[] args) {
         // First test case in Main
         System.out.println("Example 1: Make My Username");
-        System.out.println("In: first = " + "\"Sean\"" +
-                ", last = " + "\"Flannery\"");
+        System.out.println("In: first = " + "\"Oyku\"" +
+                ", last = " + "\"Sonmez\"");
         System.out.println("Out: " +
-        StringUtils.makePurdueUsername("Sean", "Flannery"));
+        StringUtils.makePurdueUsername("Oyku", "Sonmez"));
     }
 }
 
